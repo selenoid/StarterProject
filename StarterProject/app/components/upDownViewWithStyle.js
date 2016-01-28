ad21d11086d5e4a3ddf76b00320cfa22ef4956a7
İbrahim Emre Selen <emre_selen@yahoo.com>
2016-01-28 08:35:06 +0000

    Dabari Changes made by İbrahim Emre Selen (emre_selen@yahoo.com).

--- StarterProject/app/components/upDownViewWithStyle.js
+++
@@ -1,7 +0,0 @@
-var view = require("ui/core/view");
-var viewModel = require("./upDownViewModel");
-
-exports.loadUpDownView = function(args) {
-    var page = args.object;
-    page.bindingContext = viewModel;
-}
